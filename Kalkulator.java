32dba30df66b6905b3d3a08c914435bdc7d77f2e
VallenDraa <jestinevallendradwiputra@gmail.com>
2022-11-28 14:01:06 +0700

    Dinit

---
+++ Kalkulator.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+public class Kalkulator {
+  JFrame frame;
+  JTextField display;
+
+  public Kalkulator() {
+    // inisialisasi frame
+    frame = new JFrame("Kalkulator");
+    frame.setSize(400, 500);
+    frame.setVisible(true);
+  }
+}
